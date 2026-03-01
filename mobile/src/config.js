caacc7a6c377575ccb2ee7accc948c0236c44f8a
clakg <clakg@icloud.com>
2026-03-01 02:21:42 +0100

    wcorrection web/dist/

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,5 +1,11 @@
-//export const API_URL = "http://192.168.1.21:8080";
-export const API_URL = "http://10.20.144.143:8080";
+// Wi-Fi maison
+export const API_URL = "http://192.168.1.21:8080";
 
+// Wi-Fi école
+// export const API_URL = "http://10.20.144.143:8080";
 
+// Émulateur Android
+// export const API_URL = "http://10.0.2.2:8080";
 
+// PROD
+// export const API_URL = "http://100.66.190.108:8080";
